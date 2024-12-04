24547a9dceca64d83b4c3e461a31a5db33bb7dc9
ofiryag <ofiryagoda@gmail.com>
2024-12-04 22:16:07 +0200

    - updated schema max visits

--- src/contracts/schemas.ts
+++ src/contracts/schemas.ts
@@ -6,12 +6,12 @@ export const postVisitRequestSchema = z.object({
   });
 
 export const bulkVisitRequestSchema = z.object({
-    visits: z.array(postVisitRequestSchema).min(1, 'at least one visit is required'),
+    visits: z.array(postVisitRequestSchema).min(1, 'at least one visit is required').max(999, 'maximum vists exceeded'),
     organization_id: z.string().min(1, 'organization id is required'),
   });
   
   export const getVisitRequestSchema = z.object({
     organization_id: z.string(),
-    offset: z.number(),
-    limit: z.number(),
+    offset: z.number().max(999999999),
+    limit: z.number().max(999999999),
   });
